cbfceac22e19ab3f90086bd0cdd4a1acc1545b51
ryanrain2016 <398244837@qq.com>
2017-04-11 17:22:19 +0800

    CP

--- FreeEye/AlertManage/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AlertmanageConfig(AppConfig):
-    name = 'AlertManage'
--- FreeEye/FileManage/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FilemanageConfig(AppConfig):
-    name = 'FileManage'
